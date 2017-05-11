b8dd87f7f3ee0237478d9b19cdf45412e68ea06b
Brent Miller <brmiller@renovo1.com>
2017-05-11 16:01:44 -0400

    adjustments to actual animation

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const mainButton = $('.btn__main'),
 	  menu = $('.btn-group--options'),
 	  option = $('.btn__option'),
 
-	  animationTime = 0.3;
+	  animationTime = .1;
 
 var clicked;
 
@@ -37,24 +37,26 @@ tlMenu
 	.fromTo(menu, animationTime, {
 		visibility: 'hidden',
 		scale: 0,
-		bottom: 0
+		y: '-=60'
 	},
 	{
 		visibility: 'visible',
 		scale: 0.8,
-		bottom: '3em'
+		y: '-=150'
 	});
 
 tlOption
 	.staggerFromTo(option, animationTime, {
-		rotation: 45,
+		rotation: 180,
 		scale: 0,
+		opacity: 0,
 	},
 	{
 		rotation: 0,
 		scale: 0.8,
+		opacity: 1
 	},
-	-.15);
+	(animationTime * -1));
 
 // Trigger
 mainButton.click(() => {
